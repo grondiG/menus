bb6aab21a0ef5c320bfcf5efc5a2e8860b08d7dc
Arkadiusz Grondys <arkadiusz.grondys@vality.pl>
2024-03-12 17:18:02 +0100

    create working directive for showing error message

--- src/app/core/directives/error-message/error-message.directive.ts
+++ src/app/core/directives/error-message/error-message.directive.ts
@@ -1,41 +1,57 @@
-import { AfterViewInit, Directive, ElementRef, inject, Input, Renderer2 } from '@angular/core';
-import { ControlContainer, FormGroup, NgControl, NgForm } from '@angular/forms';
+import {
+  AfterViewInit, ChangeDetectorRef,
+  ComponentRef, DestroyRef,
+  Directive,
+  inject,
+  Input,
+  ViewContainerRef
+} from '@angular/core';
+import { ControlContainer, FormGroup, NgForm } from '@angular/forms';
+import { ErrorMessageComponent } from '../../components/error-message/error-message.component';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 
 @Directive({
   standalone: true,
   selector: '[appErrorMessage]'
 })
 export class ErrorMessageDirective implements AfterViewInit {
-  @Input() control: NgControl;
   private container: ControlContainer = inject(ControlContainer);
-  private errorElement!: unknown;
+  private viewContainerRef: ViewContainerRef = inject(ViewContainerRef);
+  private destroy: DestroyRef = inject(DestroyRef);
+  private cdr: ChangeDetectorRef = inject(ChangeDetectorRef);
+
+  @Input({ required: true }) set appErrorMessage(value: string) {
+    this.control = value;
+  }
+
+  private control: string;
+  private errComponent!: ComponentRef<ErrorMessageComponent>
 
   get parentForm(): NgForm {
     return this.container as NgForm;
   }
 
-  constructor(private el: ElementRef, private renderer: Renderer2) {
-    //TODO:
-    // change to factory
-    // const factory = this.componentFactoryResolver.resolveComponentFactory(LoginComponent);
-    // const component: ComponentRef<LoginComponent> = this.viewContainerRef.createComponent(factory);
-    // component.instance.user = "prop 1";
-    // component.instance.input2 = "prop 2";
-    this.errorElement = this.renderer.createElement('app-error-message');
-    const factory =
-    // this.renderer.setProperty()
-    this.renderer.addClass(this.errorElement , 'alert'); // Add any necessary classes
-    this.renderer.addClass(this.errorElement , 'alert-danger'); // Customize styling
-
-    // Set the error message content (you can customize this)
-    this.renderer.setProperty(this.errorElement , 'innerText', 'This field is required.');
-
-    // Append the error message component below the input
-    this.renderer.appendChild(this.el.nativeElement.parentNode, this.errorElement );
+  get parentFormGroup(): FormGroup {
+    return this.container.control as FormGroup;
   }
 
   ngAfterViewInit(): void {
+    this.errComponent = this.viewContainerRef.createComponent(ErrorMessageComponent);
 
+    queueMicrotask(() => this.parentFormGroup.get(this.control).valueChanges.pipe(
+      takeUntilDestroyed(this.destroy)
+    ).subscribe(() => {
+      this.updateErrorMessage();
+    }));
   }
 
+  private updateErrorMessage(): void {
+    this.errComponent.destroy();
+
+    this.errComponent = this.viewContainerRef.createComponent(ErrorMessageComponent);
+    this.errComponent.instance.errors = this.parentFormGroup.get(this.control).errors;
+    this.errComponent.instance.dirty = this.parentForm?.submitted || this.parentFormGroup.get(this.control).dirty || this.parentFormGroup.get(this.control).touched;
+
+    this.cdr.markForCheck();
+  }
 }
