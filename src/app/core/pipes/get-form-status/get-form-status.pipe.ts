a4bc7c2f3e2a47b79cc345d54aff97a370918032
grondi <arogrondi@gmail.com>
2024-03-21 21:16:55 +0100

    fix of tests

--- src/app/core/pipes/get-form-status/get-form-status.pipe.ts
+++ src/app/core/pipes/get-form-status/get-form-status.pipe.ts
@@ -1,16 +1,9 @@
 import { Pipe, PipeTransform } from '@angular/core';
-import { FormControl, NgForm, NgModel } from '@angular/forms';
+import { NgForm, NgModel } from '@angular/forms';
 
 @Pipe({ name: 'getFormStatusFor' })
 export class GetFormStatusForPipe implements PipeTransform {
   transform(form: NgForm, control: NgModel, controlName?: string): boolean {
-    const submitted: boolean = form.submitted;
-    const formControl: FormControl|undefined = form.control.get(controlName) as FormControl;
-
-    console.log(submitted);
-    console.log(formControl?.dirty);
-    console.log(formControl?.touched);
-
     return form.submitted || control.dirty || control.touched;
   }
 }
